946c76330fa4e635345512f18eb57f9f84d1fdb6
do1lka <102466894+do1lka@users.noreply.github.com>
2022-04-10 17:31:58 +0500

    Update br.py

--- br.py
+++ br.py
@@ -1,76 +1,142 @@
 from PyQt5.QtWidgets import QApplication, QLabel, QWidget, QPushButton, QLineEdit, QVBoxLayout, QHBoxLayout
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import Qt, QTimer, QTime
 
 app = QApplication([])
-#первое окно
-win = QWidget()
-win.setWindowTitle('окно 1')
-win.resize(1920, 1080)
-win.show()
-#скрытие первого окна
-def dasd():
-    win.hide()
-    win2.show()
-#первое окно 2
-label1 = QLabel('Добро пожаловать в программу по определению состояния здоровья!')
-label2 = QLabel('Тест Руфье: тут крч текст много букаф мне лень его печатать')
-button1 = QPushButton('Начать')
-v_line1 = QVBoxLayout()
-v_line1.addWidget(label1, alignment=Qt.AlignCenter)
-v_line1.addWidget(label2, alignment=Qt.AlignCenter)
-v_line1.addWidget(button1, alignment=Qt.AlignCenter)
-win.setLayout(v_line1)
-button1.clicked.connect(dasd)
-#второе окно
-win2 = QWidget()
-win2.setWindowTitle('окно 2')
-win2.resize(1920, 1080)
-win2.hide()
-#скрытие второго окна
-def dasd2():
-    win2.hide()
-    win3.show()   
-#второе окно 2
-label3 = QLabel('Текст1')
-line_edit1 = QLineEdit()
-label4 = QLabel('Текст2')
-line_edit2 = QLineEdit()
-label5 = QLabel('Текст3')
-button2 = QPushButton('Нажми меня1')
-line_edit3 = QLineEdit()
-label6 = QLabel('Текст4')
-button3 = QPushButton('Нажми меня2')
-label7 = QLabel('Текст5')
-button4 = QPushButton('Нажми меня3')
-line_edit4 = QLineEdit()
-line_edit5 = QLineEdit()
-button4 = QPushButton('След. окно')
-label_timer = QLabel('Тут время')
-v_line2 = QVBoxLayout()
-v_line2.addWidget(label3, alignment=Qt.AlignLeft)
-v_line2.addWidget(line_edit1, alignment=Qt.AlignLeft)
-v_line2.addWidget(label4, alignment=Qt.AlignLeft)
-v_line2.addWidget(line_edit2, alignment=Qt.AlignLeft)
-v_line2.addWidget(label5, alignment=Qt.AlignLeft)
-v_line2.addWidget(button2, alignment=Qt.AlignLeft)
-v_line2.addWidget(line_edit3, alignment=Qt.AlignLeft)
-v_line2.addWidget(label6, alignment=Qt.AlignLeft)
-v_line2.addWidget(button3, alignment=Qt.AlignLeft)
-v_line2.addWidget(label7, alignment=Qt.AlignLeft)
-v_line2.addWidget(line_edit4, alignment=Qt.AlignLeft)
-v_line2.addWidget(line_edit5, alignment=Qt.AlignLeft)
-v_line2.addWidget(button4, alignment=Qt.AlignCenter)
-v_line3 = QVBoxLayout()
-v_line3.addWidget(label_timer, alignment=Qt.AlignCenter)
-h_line = QHBoxLayout()
-h_line.addLayout(v_line2)
-h_line.addLayout(v_line3)
-win2.setLayout(h_line)
-button4.clicked.connect(dasd2)
-#третье окно
-win3 = QWidget()
-win3.setWindowTitle('окно 3')
-win3.resize(1920, 1080)
-win3.hide()
 
+class frs_window(QWidget):#первое окно
+    def __init__(self):
+        QWidget.__init__(self)
+        self.set_appear()
+        self.unitUI()
+        self.connects()
+        self.show()
+    def set_appear(self):
+        self.setWindowTitle('окно 1')
+        self.resize(1920, 1080)
+    #скрытие первого окна
+    def dasd(self):
+        self.hide()
+        second_window.show()
+    def unitUI(self):
+        self.label1 = QLabel('Добро пожаловать в программу по определению состояния здоровья!')
+        self.label2 = QLabel('Тест Руфье: тут крч текст много букаф мне лень его печатать')
+        self.button1 = QPushButton('Начать')
+        self.v_line1 = QVBoxLayout()
+        self.v_line1.addWidget(self.label1, alignment=Qt.AlignCenter)
+        self.v_line1.addWidget(self.label2, alignment=Qt.AlignCenter)
+        self.v_line1.addWidget(self.button1, alignment=Qt.AlignCenter)
+        self.setLayout(self.v_line1)
+    def connects(self):
+        self.button1.clicked.connect(self.dasd)
+class sec_window(QWidget):#второе окно
+    def __init__(self):
+        QWidget.__init__(self)
+        self.set_appear()
+        self.unitUI()
+        self.connects()
+    def set_appear(self):
+        self.setWindowTitle('окно 2')
+        self.resize(1920, 1080)
+    def dasd2(self):
+        self.hide()
+        third_window.show()
+    def unitUI(self): 
+        self.label3 = QLabel('Текст1')
+        self.line_edit1 = QLineEdit()
+        self.label4 = QLabel('Текст2')
+        self.line_edit2 = QLineEdit()
+        self.label5 = QLabel('Текст3')
+        self.button2 = QPushButton('Таймер 1')
+        self.line_edit3 = QLineEdit()
+        self.label6 = QLabel('Текст4')
+        self.button3 = QPushButton('Таймер2')
+        self.label7 = QLabel('Текст5')
+        self.button4 = QPushButton('Таймер3')
+        self.line_edit4 = QLineEdit()
+        self.line_edit5 = QLineEdit()
+        self.button5 = QPushButton('След. окно')
+        self.label_timer = QLabel('Тут время')
+        self.v_line2 = QVBoxLayout()
+        self.v_line2.addWidget(self.label3, alignment=Qt.AlignLeft)
+        self.v_line2.addWidget(self.line_edit1, alignment=Qt.AlignLeft)
+        self.v_line2.addWidget(self.label4, alignment=Qt.AlignLeft)
+        self.v_line2.addWidget(self.line_edit2, alignment=Qt.AlignLeft)
+        self.v_line2.addWidget(self.label5, alignment=Qt.AlignLeft)
+        self.v_line2.addWidget(self.button2, alignment=Qt.AlignLeft)
+        self.v_line2.addWidget(self.line_edit3, alignment=Qt.AlignLeft)
+        self.v_line2.addWidget(self.label6, alignment=Qt.AlignLeft)
+        self.v_line2.addWidget(self.button3, alignment=Qt.AlignLeft)
+        self.v_line2.addWidget(self.label7, alignment=Qt.AlignLeft)
+        self.v_line2.addWidget(self.line_edit4, alignment=Qt.AlignLeft)
+        self.v_line2.addWidget(self.line_edit5, alignment=Qt.AlignLeft)
+        self.v_line2.addWidget(self.button4, alignment=Qt.AlignLeft)
+        self.v_line2.addWidget(self.button5, alignment=Qt.AlignCenter)
+        self.v_line3 = QVBoxLayout()
+        self.v_line3.addWidget(self.label_timer, alignment=Qt.AlignCenter)
+        self.h_line = QHBoxLayout()
+        self.h_line.addLayout(self.v_line2)
+        self.h_line.addLayout(self.v_line3)
+        self.setLayout(self.h_line)
+    def connects(self):
+        self.button2.clicked.connect(self.timer1)
+        self.button3.clicked.connect(self.timer2)
+        self.button4.clicked.connect(self.timer3)
+        self.button5.clicked.connect(self.dasd2)
+    #таймер 1
+    def timer1(self):
+        global time
+        time = QTime(0, 0, 15)
+        self.timer = QTimer()
+        self.timer.timeout.connect(self.timeEvent)
+        self.timer.start(1000)
+
+    def timeEvent(self):
+        global time
+        time = time.addSecs(-1)
+        self.label_timer.setText(time.toString('hh:mm:ss'))
+        if time.toString('hh:mm:ss') == '00:00:00':
+            self.timer.stop()
+    def timer2(self):
+        global time
+        time = QTime(0, 0, 30)
+        self.timer = QTimer()
+        self.timer.timeout.connect(self.timeEvent2)
+        self.timer.start(2000)
+
+    def timeEvent2(self):
+        global time
+        time = time.addSecs(-1)
+        self.label_timer.setText(time.toString('hh:mm:ss')[6:8])
+        if time.toString('hh:mm:ss') == '00:00:00':
+            self.timer.stop()
+    def timer3(self):
+        global time
+        time = QTime(0, 1, 0)
+        self.timer = QTimer()
+        self.timer.timeout.connect(self.timeEvent)
+        self.timer.start(1000)
+
+    def timeEvent3(self):
+        global time
+        time = time.addSecs(-1)
+        self.label_timer.setText(time.toString('hh:mm:ss'))
+        if time.toString('hh:mm:ss') <= '00:00:59':
+            self.label_timer.setStyleSheet("color: rgb (0, 255, 0)")
+        if time.toString('hh:mm:ss') <= '00:00:45':
+            self.label_timer.setStyleSheet("color: rgb (0, 0, 0)")
+        if time.toString('hh:mm:ss') <= '00:00:15':
+            self.label_timer.setStyleSheet("color: rgb (0, 255, 0)")
+        if time.toString('hh:mm:ss') == '00:00:00':
+            self.timer.stop()
+class thr_window(QWidget):#третье окно
+    def __init__(self):
+        QWidget.__init__(self)
+        self.set_appear()
+    def set_appear(self):
+        self.setWindowTitle('окно 3')
+        self.resize(1920, 1080)
+
+first_window = frs_window()
+second_window = sec_window()
+third_window = thr_window()
 app.exec_()
